a536d992114144dcab4dd703e09f4240269b834a
Shuvo <shovocse753@gmail.com>
2019-12-13 01:18:11 +0600

    Interface changed.

--- UserInterface.php
+++ UserInterface.php
@@ -13,9 +13,9 @@
       <fieldset>
         <legend>Notification::</legend>
         <label>Title</label></br>
-        <input type="text" name="title" class="pure-input-1-2" placeholder="Notification title"></br></br>
+        <input type="text" name="title" class="title_box" placeholder="Notification title"></br></br>
         <label>Message</label></br>
-        <input type="text" name="message" class="pure-input-1-2" placeholder="Notification message"></br></br>
+        <input type="text" name="message" class="message_box" placeholder="Notification message"></br></br>
         <button type="submit" class="pure-button pure-button-primary send_button">Send</button>
       </fieldset>
     </form>
